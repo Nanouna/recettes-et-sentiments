419da0e7a211865de8f32f846f74ce9c06cc7bf2
Thomas Manson <wagon@mansonthomas.com>
2024-06-10 17:18:27 +0200

    setup instruction for google cloud Artifcat Registry

---
+++ setup-gcloud-artifcat-registry.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+GCP_REGION=$(yq e '.GCP_REGION' $ENV_FILE)
+gcloud auth configure-docker $GCP_REGION-docker.pkg.dev
+
+DOCKER_REPO_NAME=recettes-et-sentiments
+gcloud artifacts repositories create $DOCKER_REPO_NAME \
+  --repository-format=docker \
+  --location=$GCP_REGION  \
+  --description="Docker images in GAR"
