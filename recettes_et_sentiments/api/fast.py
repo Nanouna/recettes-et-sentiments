79a02f373f2722293391e9326552d06f10f9b6f5
Thomas Manson <wagon@mansonthomas.com>
2024-06-10 17:01:42 +0200

    add a  prediction function, disable uncompressing (.pkl is already compressed)

--- recettes_et_sentiments/api/fast.py
+++ recettes_et_sentiments/api/fast.py
@@ -2,6 +2,7 @@ import pandas as pd
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from recettes_et_sentiments.api_model.registry import load_model
+from recettes_et_sentiments.api_model.FAST_model_variant import find_recipie_with_similar_elements_model_fast
 
 
 
@@ -16,10 +17,19 @@ app.add_middleware(
     allow_headers=["*"],  # Allows all headers
 )
 
-model = load_model()
-# assert model is not None
-# app.state.model = model
 
+app.state.model_fast = load_model("model_fast")
+app.state.recipe_processed = pd.read_parquet("/tmp/data/preproc_recipes_fast_name-tag-desc-ingredients.parquet")
+
+@app.get("/model_fast")
+def model_fast(query:str):
+    # in real world, we would check the input
+
+    result = find_recipie_with_similar_elements_model_fast(query=query, model_fast=app.state.model_fast, recipe_processed=app.state.recipe_processed)
+    return {
+        'query': query,
+        'result': result.to_json()
+    }
 
 
 
