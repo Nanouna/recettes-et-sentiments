79458e70d39244c3de48d67bec15d4a1c6bc244c
Thomas Manson <wagon@mansonthomas.com>
2024-06-10 18:21:02 +0200

    add caching layer for model (KNN, W2VEC) and dataframe

--- recettes_et_sentiments/api_model/W2V_model.py
+++ recettes_et_sentiments/api_model/W2V_model.py
@@ -1,3 +1,5 @@
+import os
+import joblib
 from gensim.models import Word2Vec
 import logging
 import numpy as np
@@ -6,6 +8,7 @@ from sklearn.neighbors import NearestNeighbors
 import typing
 
 from recettes_et_sentiments.api_model import parameters
+from recettes_et_sentiments.api_model.registry import load_model, save_model
 
 
 logger = logging.getLogger(__name__)
@@ -51,17 +54,40 @@ def preprocess_data(data: pd.DataFrame, column_to_process: str)-> typing.Union[p
     '''
     Compilation function of training and vectorization
     '''
-    word2vec_model = train_word2vec(data[column_to_process])
-    data[column_to_process+'_vector'] = embedding(word2vec_model, data[column_to_process].tolist())
+
+    word2vec_model_cache_path = f"w2vec_model_{column_to_process}"
+
+    word2vec_model = load_model(word2vec_model_cache_path)
+
+    if word2vec_model is None:
+        word2vec_model = train_word2vec(data[column_to_process])
+        save_model(word2vec_model_cache_path)
+
+
+    word2vec_df_cache_path = f"/tmp/data/w2vec_df_{column_to_process}.parquet"
+    if os.path.exists(word2vec_df_cache_path):
+        data = pd.read_parquet(word2vec_df_cache_path)
+    else:
+        data[column_to_process+'_vector'] = embedding(word2vec_model, data[column_to_process].tolist())
+        data.to_parquet(word2vec_df_cache_path)
+
     return data, word2vec_model
 
-def instantiate_model(data: pd.DataFrame, processed_col:str):
+def instantiate_model(data: pd.DataFrame, column_to_process:str):
     '''
     Instantiation of KNN model on recipes
     '''
-    neigh = NearestNeighbors(n_neighbors=2, radius=0.4)
-    neigh.fit(np.array(data[processed_col+'_vector'].tolist()))
-    return neigh
+
+    knn_model_cache_path = f"knn_model_{column_to_process}"
+
+    knn_model = load_model(knn_model_cache_path)
+
+    if knn_model is None:
+        knn_model = NearestNeighbors(n_neighbors=2, radius=0.4)
+        knn_model.fit(np.array(data[column_to_process+'_vector'].tolist()))
+        save_model(knn_model_cache_path)
+
+    return knn_model
 
 def recommend_recipe_from_another(model, data: pd.DataFrame, processed_col:str, entry_recipe_id: int) -> pd.DataFrame:
     """
@@ -73,3 +99,16 @@ def recommend_recipe_from_another(model, data: pd.DataFrame, processed_col:str, 
     if len(indices[0]) < 2:
         return None
     return data.iloc[indices[0][1]]
+
+if __name__ == "__main__":
+
+
+
+    data = pd.read_parquet('../batch-1672-recettes-et-sentiments-data/last_preproc_we_hope.parquet')
+
+    recipes_with_vectors, word2vec_model = preprocess_data(data, 'tags')
+    KNN_model = instantiate_model(recipes_with_vectors, 'tags')
+    recommended_recipe = recommend_recipe_from_another(KNN_model, recipes_with_vectors, 'tags', entry_recipe_id=308080)
+
+
+    print(recommended_recipe)
